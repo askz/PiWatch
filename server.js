b1624a907af76a822c0c5bce2c27af58bb5c639f
askz <askzz@me.com>
2015-03-07 15:29:07 +0100

    updates from scratch

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
+#!/usr/bin/env node
 var requirejs = require('requirejs');
 
 requirejs.config({
     nodeRequire: require
 });
-
-var http = require('http');
-var httpProxy = require('http-proxy');
-
-
 
 requirejs([
     'http',
@@ -19,6 +15,7 @@ requirejs([
 
 function(
     http,
+    httpProxy,
     express,
     socketio,
     fs,
@@ -27,10 +24,9 @@ function(
     var clients = 0,
         motionConfFile = __dirname + '/motion.conf';
 
-   	startMotion();
 
    	require('dns').lookup(require('os').hostname(), function (err, ip, fam) {
-		var target = 'http://' + ip +':8081';
+		var target = 'http://' + '127.0.0.1' +':8081';
 		httpProxy.createProxyServer({target:target}).listen(8080);
 		console.log("Started at " + target);
 	})
@@ -61,7 +57,11 @@ function(
         var motion = exec('motion -c ' + motionConfFile + ' &', function(error, stdout, stderr) {
             if(error) { return; }
             console.log(motion.pid);
+	    console.log("Motion started;");	    
         });
     }
+    
+     startMotion();
+
 
 });
